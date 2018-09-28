72f9e22832cbe705f956f64c97a652e87bc2c0af
kozistr <kozistr@gmail.com>
2018-09-29 01:57:09 +0900

    feature: metrics, #2

---
+++ metric.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+
+def psnr(x, y):
+    return tf.image.psnr(a=x, b=y, max_val=255.)
+
+
+def ssim(x, y):
+    return tf.image.ssim(img1=x, img2=y, max_val=255.)
+
+
+def mse(x, y):
+    return tf.metrics.mean_squared_error(labels=x, predictions=y)
