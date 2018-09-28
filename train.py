d7d1064c6a6a5abbef773ab2e3019851d4c2e444
kozistr <kozistr@gmail.com>
2018-09-29 04:40:49 +0900

    update: training part

--- train.py
+++ train.py
@@ -1,13 +1,39 @@
-from dataset import Div2KDataSet
+from dataset import Div2KDataSet as DataSet
+from dataset import DataIterator
+from config import get_config
 
 import tensorflow as tf
-import numpy as np
+import time
+
 import model
 
-import config
+# Configuration
+config, _ = get_config()
 
 
 def main():
+    start_time = time.time()  # Clocking start
+
+    # Div2K - Track 1: Bicubic downscaling - x4 DataSet load
+    ds = DataSet(ds_path="D://DataSet/DIV2K/",
+                 ds_name="X4",
+                 use_save=True,
+                 save_type="to_h5",
+                 save_file_name="D://DataSet/DIV2K/DIV2K",
+                 use_img_scale=False)
+    """
+    ds = DataSet(ds_hr_path="D://DataSet/DIV2K/DIV2K-hr.h5",
+                 ds_lr_path="D:///DataSet/DIV2K/DIV2K-lr.h5",
+                 use_img_scale=False)
+    """
+
+    hr, lr = ds.hr_images, ds.lr_images  # [0, 255] scaled images
+
+    print("[+] Loaded HR image ", hr.shape)
+    print("[+] Loaded LR image ", lr.shape)
+
+    # DataIterator
+    di = DataIterator(lr, hr, config.batch_size)
 
     # gpu config
     gpu_config = tf.GPUOptions(allow_growth=True)
@@ -35,6 +61,50 @@ def main():
                                 opt_eps=config.opt_epsilon,
                                 tf_log=config.summary,
                                 )
+
+        # Initializing
+        sess.run(tf.global_variables_initializer())
+
+        # Load model & Graph & Weights
+        global_step = 0
+
+        ckpt = tf.train.get_checkpoint_state(config.summary)
+        if ckpt and ckpt.model_checkpoint_path:
+            # Restores from checkpoint
+            rcan_model.saver.restore(sess, ckpt.model_checkpoint_path)
+
+            global_step = int(ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1])
+            print("[+] global step : %d" % global_step, " successfully loaded")
+        else:
+            print('[-] No checkpoint file found')
+
+        # config params
+        lr = config.lr if global_step < config.lr_decay_step \
+            else config.lr * (config.lr_decay * (global_step // config.lr_decay_step))
+
+        rcan_model.global_step.assign(tf.constant(global_step))
+        start_epoch = global_step // (ds.n_images // config.batch_size)
+
+        for epoch in range(start_epoch, config.epochs):
+            for x_lr, x_hr in di.iterate():
+                # training
+                _, loss = sess.run([rcan_model.opt, rcan_model.loss],
+                                   feed_dict={
+                                       rcan_model.x_lr: x_lr,
+                                       rcan_model.x_hr: x_hr,
+                                       rcan_model.lr: lr,
+                                   })
+
+                if global_step % config.logging_step == 0:
+                    print("[+] %d epochs %d steps" % (epoch, global_step), "loss : {.8f}".format(loss))
+
+                # increase global step
+                rcan_model.global_step.assign_add(tf.constant(1))
+
+    end_time = time.time() - start_time  # Clocking end
+
+    # Elapsed time
+    print("[+] Elapsed time {:.8f}s".format(end_time))
 
 
 if __name__ == "__main__":
