f9e7eb1c586c12fac770024e38e3ad2e0585fd71
kozistr <kozistr@gmail.com>
2018-09-29 00:25:51 +0900

    feature: RCAB (Residual Channel Attention Block) implementation, #1

--- model.py
+++ model.py
@@ -8,11 +8,14 @@ class RCAN:
                  sess,                 # Tensorflow session
                  batch_size=16,        # batch size
                  reduction=16,         # reduction rate at CA layer
+                 eps=1.1e-5,
                  ):
         self.sess = sess
         self.batch_size = batch_size
 
         self.reduction = reduction
+
+        self._eps = eps
 
     @staticmethod
     def channel_attention(x, f, reduction, name):
@@ -24,7 +27,7 @@ class RCAN:
         :param name: scope name
         :return: output layer
         """
-        with tf.variable_scope("channel_attention-%s" % name):
+        with tf.variable_scope("CA-%s" % name):
             x_gap = tfutil.adaptive_global_average_pool_2d(x)
 
             x = tfutil.conv2d(x_gap, f=f // reduction, k=1, pad='VALID')
@@ -33,3 +36,19 @@ class RCAN:
             x = tfutil.conv2d(x, f=f, k=1, pad='VALID')
             x = tf.nn.sigmoid(x)
             return x_gap * x
+
+    def residual_channel_attention_block(self, x, f, kernel_size, reduction, use_bn, name):
+        with tf.variable_scope("RCAB-%s" % name):
+            x = tfutil.conv2d(x, f=f, k=kernel_size, pad='VALID')
+            x = tf.layers.batch_normalization(epsilon=self._eps) if use_bn else x
+            x = tf.nn.relu(x)
+
+            x = tfutil.conv2d(x, f=f, k=kernel_size, pad='VALID')
+            res = tf.layers.batch_normalization(epsilon=self._eps) if use_bn else x
+
+            x = self.channel_attention(x, f, reduction, name)
+            return res + x
+
+    @staticmethod
+    def residual_group(x, f, reduction, name):
+        pass
