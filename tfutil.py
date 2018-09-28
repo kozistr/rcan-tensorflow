7bf4298d45b9864813dd8ad14c59c8912debfb04
kozistr <kozistr@gmail.com>
2018-09-29 02:46:57 +0900

    feature: activation functions

--- tfutil.py
+++ tfutil.py
@@ -3,15 +3,17 @@ import tensorflow as tf
 
 SEED = 1337
 
-reg = 5e-4
-eps = 1.1e-5
-
 tf.set_random_seed(SEED)
 
 w_init = tf.contrib.layers.variance_scaling_initializer(factor=1., mode='FAN_AVG', uniform=True)
 b_init = tf.zeros_initializer()
 
+reg = 5e-4
 w_reg = tf.contrib.layers.l2_regularizer(reg)
+
+
+# ---------------------------------------------------------------------------------------------
+# Functions
 
 
 def adaptive_global_average_pool_2d(x):
@@ -30,6 +32,7 @@ def conv2d(x, f=64, k=3, s=1, pad='SAME', use_bias=True, reuse=None, name='conv2
     :param k: kernel size
     :param s: strides
     :param pad: padding
+    :param use_bias: using bias or not
     :param reuse: reusable
     :param name: scope name
     :return: output
@@ -43,3 +46,24 @@ def conv2d(x, f=64, k=3, s=1, pad='SAME', use_bias=True, reuse=None, name='conv2
                             use_bias=use_bias,
                             reuse=reuse,
                             name=name)
+
+
+def pixel_shuffle(x, f, scaling_factor):
+    x = conv2d(x, f)
+    return x
+
+
+# ---------------------------------------------------------------------------------------------
+# Activation Functions
+
+
+def relu(x):
+    return tf.nn.relu
+
+
+def leaky_relu(x, alpha=.2):
+    return tf.nn.leaky_relu(x, alpha)
+
+
+def elu(x):
+    return tf.nn.elu(x)
